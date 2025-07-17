4c9a615acde99cda9f1d6a02d83d7e636f90baeb
kyoungmoon <baekm1105@gmail.com>
2025-07-17 17:11:24 +0900

    spring_framework end

---
+++ julspringweb/src/main/java/com/example/julspringweb/JulspringwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.julspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulspringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulspringwebApplication.class, args);
+	}
+
+}
