bad10a03cd470c9fb2457655eb15e96265dd2d08
kyoungmoon <baekm1105@gmail.com>
2025-07-21 18:14:03 +0900

    day0721 work

---
+++ julspringmybatis/src/main/java/com/example/controller/DeptController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.dao.DeptDao;
+import com.example.dto.Dept;
+import com.google.gson.Gson;
+
+@RestController
+public class DeptController {
+
+  @Autowired
+  DeptDao dao; // 구현 객체 주입
+
+  @GetMapping("dept")
+  public String getMethodName() {
+    Gson gson = new Gson();
+    List<Dept> dlist = dao.deptAll();
+
+    return gson.toJson(dlist);
+  }
+
+}
---
+++ julspringmybatis/src/main/java/com/example/dao/DeptDao.java
@@ -0,0 +1,16 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.dto.Dept;
+
+@Mapper
+public interface DeptDao {
+
+  @Select("select * from dept")
+  List<Dept> deptAll();
+  
+}
